724a12e4045bb215c6d14512e87b33561c54d075
Anthony Arnold <stubbiegaming@gmail.com>
2023-08-11 17:55:41 -0400

    fixed grid sizing

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const gridMap = document.querySelector('.grid');
 
 function createGrid(size){
-    for(let i = 0; i <= size; i++){
+    for(let i = 0; i < size; i++){
         const block = document.createElement('div');
         block.classList.add('block');
         gridMap.appendChild(block);
