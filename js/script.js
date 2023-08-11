bdcacbe8b45a6d9a0977702c210dc99dc104be6e
Anthony Arnold <stubbiegaming@gmail.com>
2023-08-11 17:49:49 -0400

    added grid functionality

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const gridMap = document.querySelector('.grid');
+
+function createGrid(size){
+    for(let i = 0; i <= size; i++){
+        const block = document.createElement('div');
+        block.classList.add('block');
+        gridMap.appendChild(block);
+    }
+}
+createGrid(256);
